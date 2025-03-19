495e687e0cae5735d69946f4d4efafb0eed70b4d
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2025-03-19 08:09:57 -0300

    Add files via upload

---
+++ Questão 1/Models/Conta.cs
@@ -0,0 +1,61 @@
+class Conta
+{
+    public int NumeroConta { get; protected set; }
+    public string Titular { get; protected set; }
+    public double Saldo { get; protected set; }
+
+    public Conta(int numeroConta, string titular, double saldo)
+    {
+        SetNumeroConta(numeroConta);
+        SetTitular(titular);
+        SetSaldo(saldo);
+    }
+
+    public void SetNumeroConta(int numeroConta)
+    {
+        if (numeroConta < 0)
+        {
+            throw new Exception("Número da conta inválido");
+        }
+        NumeroConta = numeroConta;
+    }
+    public void SetTitular(string titular)
+    {
+        if (string.IsNullOrWhiteSpace(titular) && titular.Length > 255)
+        {
+            throw new Exception("Nome de titular inválido");
+        }
+        Titular = titular;
+    }
+    public void SetSaldo(double saldo)
+    {
+        if (saldo < 0)
+        {
+            throw new Exception("O saldo da conta é inválido");
+        }
+        Saldo = saldo;
+    }
+
+    public void Depositar(double deposito)
+    {
+        if (deposito < 0)
+        {
+            Console.WriteLine("Valor de depósito inválido");
+        }
+        else
+        {
+            Saldo += deposito;
+        }
+    }
+    private void Sacar(double saque)
+    {
+        if (saque < 0)
+        {
+            Console.WriteLine("Valor de saque Inválido");
+        }
+        else
+        {
+            Saldo -= saque;
+        }
+    }
+}
