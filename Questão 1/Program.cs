780c200d1a9a8aedebc36c8c7a03861b3b40daba
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2025-03-19 08:51:22 -0300

    Update Program.cs

--- Questão 1/Program.cs
+++ Questão 1/Program.cs
@@ -1,7 +1,39 @@
+using ContaBancaria.Models;
 class Program
 {
+    static List<Conta> contas = new List<Conta>();
     static void Main(string[] args)
     {
-        
+        Conta c = new Conta(1, "Henrique");
+        contas.Add(c);
+
+
+
     }
+
+    static void AGastadora()
+    {
+        if (contas[0].Saldo >= 10)
+        {
+            contas[0].Sacar(10);
+        }
+        Thread.Sleep(3000);
+    }
+
+    // public void AEsperta()
+    // {
+    //     if (Saldo >= 50)
+    //     {
+    //         Saldo -= 50;
+    //     }
+    //     Thread.Sleep(6000);
+    // }
+    // public void AEconomica()
+    // {
+    //     if (Saldo >= 5)
+    //     {
+    //         Saldo -= 5;
+    //     }
+    //     Thread.Sleep(12000);
+    // }
 }
